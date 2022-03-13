0b60a475af2044ad06cf1ba4c4a980c8459f9d42
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-03-13 14:10:15 -0400

    Add script to process metadata

---
+++ scripts/meta.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+
+#SBATCH --account=PAS0471
+#SBATCH --output=slurm-meta-%j.out
+
+## Report
+message("## Starting script meta.R")
+Sys.time()
+message()
+
+## Load packages
+if (!"pacman" %in% installed.packages()) install.packages("pacman")
+pkgs <- c("tidyverse")
+pacman::p_load(pkgs, character.only = TRUE)
+
+## Process command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+infile <- args[1]      # infile <- "metadata/meta_raw.tsv"
+outfile <- args[2]     # outfile <- "metadata/meta.tsv"
+
+## Read and process raw metadata
+meta <- read_tsv(infile, col_names = c("s_nr", "well", "sampleID")) %>%
+  mutate(sampleID = gsub(" |\\+", "-", sampleID),  # Replace spaces and + by underscores
+         # There is a weird mismatch in the water samples S-numbers and filename nrs,
+         # so we add 2:
+         sampleID = ifelse(sampleID %in% c("water", "Water"),
+                           paste0(s_nr + 2, "-Water"), sampleID),
+         treatment = gsub("\\d", "", sampleID),
+         treatment = gsub("-$|^-", "", treatment),
+         neg_control = ifelse(treatment == "Water", TRUE, FALSE)) %>%
+  arrange(sampleID) %>%
+  select(sampleID, s_nr, treatment, neg_control)
+
+## Print number of samples
+message("\n## Total nr of samples: ", nrow(meta))
+message("## Nr of samples by treatment:")
+print(meta %>% count(treatment))
+message("\n## Showing head of meta df:")
+print(head(meta))
+
+## Write output file
+write_tsv(meta, outfile)
+
+## Report
+message("\n## Done with script meta.R")
+Sys.time()
+message()
